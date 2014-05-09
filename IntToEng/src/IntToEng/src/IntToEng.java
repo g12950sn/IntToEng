2bae82aeecb8b0ef96eff9e854165f6cae6c4269
g12950sn <g12950sn@s30429.tsuda.ac.jp>
2014-05-09 11:31:54 +0900

    中身かえました（とがさき）

--- IntToEng/src/IntToEng/src/IntToEng.java
+++ IntToEng/src/IntToEng/src/IntToEng.java
@@ -11,7 +11,28 @@
 	
 	
 	static String translateEng(int n){
-		
-		return "n";
+		String [] keta1 = new String[20];
+		keta1[0] = "zero"; 
+				keta1[1] = " one";
+				keta1[2] = " two";
+				keta1[3] = " three";
+				keta1[4] = " four";
+				keta1[5] = " five";
+				keta1[6] = " six";
+				keta1[7] = " seven";
+				keta1[8] = " eight";
+				keta1[9] = " nine";
+				keta1[10] = " ten";
+				keta1[11] = " eleven";
+				keta1[12] = " twelve";
+				keta1[13] = " thirteen";
+				keta1[14] = " fourteen";
+				keta1[15] = " fifteen";
+				keta1[16] = " sixteen";
+				keta1[17] = " seventeen";
+				keta1[18] = " eighteen";
+				keta1[19] = " nineteen";
+				
+		return n+" "+keta1[n];
 	}
 	}
