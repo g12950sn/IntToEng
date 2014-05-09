fffda66bc99700aa475b0e47fc96efcbba6e8a85
g12950sn <g12950sn@s30429.tsuda.ac.jp>
2014-05-09 11:15:20 +0900

    5-1

---
+++ IntToEng/src/IntToEng/src/IntToEng.java
@@ -0,0 +1,17 @@
+	package IntToEng.src;
+	
+	import java.util.Scanner;
+	
+	public class IntToEng {
+		public static void main(String[] args){
+			Scanner sc = new Scanner(System.in);
+			int input = sc.nextInt();
+			System.out.println(translateEng(input));
+		}
+	
+	
+	static String translateEng(int n){
+		
+		return "n";
+	}
+	}
